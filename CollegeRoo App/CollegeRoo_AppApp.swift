50fd13b11bd9744d1292680cda9bf7342bc1e8df
Anya Mathur <bookish2710@gmail.com>
2023-08-16 10:04:33 -0400

    Initial Commit

---
+++ CollegeRoo App/CollegeRoo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollegeRoo_AppApp.swift
+//  CollegeRoo App
+//
+//  Created by scholar on 8/16/23.
+//
+
+import SwiftUI
+
+@main
+struct CollegeRoo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
